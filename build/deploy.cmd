3febd0af010a4a1018695a7a5550823695b99ec2
Hamid Shahid <Hamid.Shahid@ericsson.com>
2018-06-06 00:05:27 -0700

    overwrite file while deploying

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -4,11 +4,11 @@ call npm run build
 call cd %DEPLOYMENT_TARGET%
 for /F "delimts=" %%i in ('dir /b') do (rmdir "%%i" /s /q || del "%%i" /s /q)
 
-xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
-xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html* /s /i
-xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js* /s /i
-xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json* /s /i
-xcopy /d %DEPLOYMENT_SOURCE%\build\web.config %DEPLOYMENT_TARGET%\web.config* /s /i
+xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /y
+xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html* /s /y
+xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js* /s /y
+xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json* /s /y
+xcopy /d %DEPLOYMENT_SOURCE%\build\web.config %DEPLOYMENT_TARGET%\web.config* /s /y
 
 call npm install --only=production
 echo Deployed.
