41bc7a6dbc9a7934170c4d81917e2fc083e93447
Hamid Shahid <Hamid.Shahid@ericsson.com>
2018-06-06 00:35:23 -0700

    Fixed error in custom deployment script

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -2,13 +2,13 @@ call npm install
 call npm run build
 
 call cd %DEPLOYMENT_TARGET%
-for /F "delimts=" %%i in ('dir /b') do (rmdir "%%i" /s /q || del "%%i" /s /q)
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 
 xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /y
-xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html* /s /y
-xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js* /s /y
-xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json* /s /y
-xcopy /d %DEPLOYMENT_SOURCE%\build\web.config %DEPLOYMENT_TARGET%\web.config* /s /y
+xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html* /y
+xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js* /y
+xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*  /y
+xcopy /d %DEPLOYMENT_SOURCE%\build\web.config %DEPLOYMENT_TARGET%\web.config* /y
 
 call npm install --only=production
-echo Deployed.
+echo "Deployed."
