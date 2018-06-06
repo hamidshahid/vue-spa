79fd88f046611a87549916d39ad7ca512e5e6abe
Hamid Shahid <Hamid.Shahid@ericsson.com>
2018-06-06 01:47:00 -0700

    making server start with devserver

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -1,14 +1,4 @@
 call npm install
-call npm run build
+call npm start
 
-call cd %DEPLOYMENT_TARGET%
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
-
-echo D | xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /y
-xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html* /y
-xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js* /y
-xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*  /y
-xcopy /d %DEPLOYMENT_SOURCE%\build\web.config %DEPLOYMENT_TARGET%\web.config* /y
-
-call npm install --only=production
 echo "Deployed."
