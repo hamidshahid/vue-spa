09924e93ddef6278c45e9d0a4b9846405dbec499
Hamid Shahid <Hamid.Shahid@ericsson.com>
2018-06-07 00:07:01 -0700

    Set sever to start on port 3000

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('*', (req, res) =>{
   res.end()
 });
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
